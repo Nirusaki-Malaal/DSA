20dbfce22bf8a504094106dc49bbf837ce9d3cd2
Mradul Kumar <mradulkumar07ocb@gmail.com>
2025-09-18 19:56:38 +0530

    Create bubble_sort.java

---
+++ bubble_sort.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class program
+  {
+    public static void main (String [] args)
+    {
+      int arr[] = {23 , 53 , 34 ,4 -2}
+      for(int i = 0 ; i < arr.length-1 ; i++)
+        {
+          for (int j = 0 ; j < arr.length-n-1; j++)
+            {
+              if (arr[j]> arr[j+1])
+              {
+                arr [j] = arr[j] +arr[j+1]
+                arr[j+1]= arr[j]-arr[j+1]
+                arr[j] = ar[j] - arr[j+1]
+              }
+            }
+        }
+    }
+  }
