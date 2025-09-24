f3ac044ebdc17dbe30662e6dcc4ae75c177cc635
Mradul Kumar <mradulkumar07ocb@gmail.com>
2025-09-24 18:05:18 +0530

    Create 2ndlargestnumberwithoutsort.cpp

---
+++ 2ndlargestnumberwithoutsort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include <climits>
+int main()
+{   
+    int arr[]  = {7,1,5,4,6,2};
+    int max1=INT_MIN;
+    int max2=INT_MIN;
+    for(int i = 0 ; i < 6 ; i++)
+    {
+        if( arr[i] > max1)
+        {
+            max2 = max1;
+            max1 = arr[i];
+        }
+        else if (arr[i] > max2)
+        {
+            max2=arr[i];
+        }
+        
+    }
+    cout << max2;
+    return 0; 
+}
