fdadd84c50279426c2bed87189108f4d3ba14053
Mradul Kumar <mradulkumar07ocb@gmail.com>
2025-09-25 21:18:25 +0530

    Create arraynotnumber.cpp

---
+++ arraynotnumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,2,3,4,6};
+    for (int i = 0 ; i < 4 ; i++ )
+    {
+        if (arr[i]+1 != arr[i+1])
+             cout << arr[i] +1 ;
+    }
+    return 0;
+}
